416bafcfd6e40a85f923c104e5fcce034b71cf79
Mandeepkaur2109 <102780834+Mandeepkaur2109@users.noreply.github.com>
2022-04-05 10:00:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/contactlist/ModelClass.kt
@@ -0,0 +1,3 @@
+package com.example.contactlist
+
+data class ModelClass(var name:String,var contact :String)
---
+++ app/src/main/java/com/example/contactlist/Recycler.kt
@@ -0,0 +1,41 @@
+package com.example.recyclertask
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contactlist.ModelClass
+import com.example.contactlist.R
+
+import org.w3c.dom.Text
+
+class Recycler(var context: Context,var arrayList: ArrayList<ModelClass>):
+    RecyclerView.Adapter<Recycler.AdapterHolder>() {
+    class AdapterHolder(view: View): RecyclerView.ViewHolder(view){
+       var showname : TextView = view.findViewById(R.id.showname)
+       var showcontact : TextView = view.findViewById(R.id.showcontact)
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Recycler.AdapterHolder {
+        var itemView = LayoutInflater.from(parent.context).inflate(R.layout.contactlayout,parent,false)
+        return AdapterHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: Recycler.AdapterHolder, position: Int) {
+        holder.showname.setText("${arrayList.get(position).name}")
+        holder.showcontact.setText("${arrayList.get(position).contact}")
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+
+}
